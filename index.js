f5e776f8c9248f1a11c7fcb207277cbd33d23053
werliton <werlitoncarlos@gmail.com>
2019-10-27 22:16:44 -0300

    Using Edges and Connection: one-to-one and one-to-many

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require("apollo-server")
+const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
     enum PhotoCategory{
@@ -14,6 +14,13 @@ const typeDefs = `
         name: String!
         description: String!
         category: PhotoCategory!
+        postedBy: User!
+    }
+    type User {
+        githublogin: ID!
+        name: String!
+        avatar: String
+        postedPhotos: [Photo!]!
     }
     input PostPhotoInput{
         name: String!
@@ -23,6 +30,7 @@ const typeDefs = `
     type Query{
         totalPhotos: Int!
         allPhotos: [Photo!]!
+        allUsers: [User!]!
     }
 
     type Mutation{
@@ -30,27 +38,63 @@ const typeDefs = `
     }
 `;
 
-var _id = 0
-var photos = []
+var _id = 0;
+var users = [
+    {"githublogin":"mHattrup", "name":"Mike Hattrup"},
+    {"githublogin":"gPlake", "name":"Glen Plake"},
+    {"githublogin":"sSchimidt", "name":"Scot Schimidt"},
+]
+var photos = [
+    {
+        id:1,
+        name:'Dropping the Heart Chuete',
+        description: 'The heart chute is one of my favorite chutes',
+        category: 'ACTION',
+        githublogin: "gPlake"
+    },
+    {
+        id:2,
+        name:'Photo uploaded for Glen Plake',
+        description: 'The heart chute is one of my favorite chutes',
+        category: 'SELFIE',
+        githublogin: "gPlake"
+    },
+    {
+        id:3,
+        name:'Any person do it',
+        description: 'The heart chute is one of my favorite chutes',
+        category: 'LANDSCAPE',
+        githublogin: "sSchimidt"
+    },
+];
 
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
-    allPhotos: () => photos
+    allPhotos: () => photos,
+    allUsers: () => users
   },
   Mutation: {
-    postPhoto(root, params){
-        let newPhoto = {
-            id: _id++,
-            ...params.input
-        }
-        photos.push(newPhoto)
-        
-        return newPhoto
+    postPhoto(root, params) {
+      let newPhoto = {
+        id: _id++,
+        ...params.input
+      };
+      photos.push(newPhoto);
+
+      return newPhoto;
     }
   },
-  Photo:{
-      url: root => `http://letox.com/img/${root.id}.jpg`
+  Photo: {
+    url: root => `http://letox.com/img/${root.id}.jpg`,
+    postedBy: root => {
+        return users.find(u => u.githublogin === root.githublogin)
+    }
+  },
+  User: {
+      postedPhotos: root => {
+          return photos.filter(p => p.githublogin === root.githublogin)
+      }
   }
 };
 
