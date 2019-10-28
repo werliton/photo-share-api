7e5cefd7deb2655e781e48199b91a3ffb44251ec
werliton <werlitoncarlos@gmail.com>
2019-10-27 21:21:39 -0300

    Get all photo mutation

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const { ApolloServer } = require("apollo-server")
 
 const typeDefs = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String!
+    }
     type Query{
         totalPhotos: Int!
+        allPhotos: [Photo!]!
     }
 
     type Mutation{
-        postPhoto(name: String!, description: String): Boolean!
+        postPhoto(name: String!, description: String): Photo!
     }
 `;
 
+var _id = 0
 var photos = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
   Mutation: {
     postPhoto(root, params){
-        photos.push(params)
-        return true
+        let newPhoto = {
+            id: _id++,
+            ...params
+        }
+        photos.push(newPhoto)
+        
+        return newPhoto
     }
   }
 };
