e887d06cd1537ec51bffd8dc0b3eff9bd66a338a
werliton <werlitoncarlos@gmail.com>
2019-10-30 12:59:31 -0300

    Instalando e configurando servidor express

--- index.js
+++ index.js
@@ -1,123 +1,26 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const expressPlayground = require('graphql-playground-middleware-express').default
+const { readFileSync } = require('fs')
 
-const typeDefs = `
-    enum PhotoCategory{
-        SELFIE
-        PORTRAIT
-        ACTION
-        LANDSCAPE
-        GRAPHIC
-    }
-    type Photo {
-        id: ID!
-        url: String!
-        name: String!
-        description: String!
-        category: PhotoCategory!
-        postedBy: User!
-        taggedUsers: [User!]!
-    }
-    type User {
-        githublogin: ID!
-        name: String!
-        avatar: String
-        postedPhotos: [Photo!]!
-        inPhotos: [Photo!]!
-    }
-    input PostPhotoInput{
-        name: String!
-        category: PhotoCategory=PORTRAIT
-        description: String
-    }
-    type Query{
-        totalPhotos: Int!
-        allPhotos: [Photo!]!
-        allUsers: [User!]!
-    }
+const typeDefs = readFileSync('./typeDefs.graphql', 'UFT-8')
+const resolvers = require('./resolvers')
 
-    type Mutation{
-        postPhoto(input: PostPhotoInput!): Photo!
-    }
-`;
-
-var _id = 0;
-var users = [
-    {"githublogin":"mHattrup", "name":"Mike Hattrup"},
-    {"githublogin":"gPlake", "name":"Glen Plake"},
-    {"githublogin":"sSchimidt", "name":"Scot Schimidt"},
-]
-var tags = [
-    {"photoId":"1", "userId":"gPlake"},
-    {"photoId":"3", "userId":"sSchimidt"},
-    {"photoId":"3", "userId":"mHattrup"},
-]
-var photos = [
-    {
-        id:1,
-        name:'Dropping the Heart Chuete',
-        description: 'The heart chute is one of my favorite chutes',
-        category: 'ACTION',
-        githublogin: "gPlake"
-    },
-    {
-        id:2,
-        name:'Photo uploaded for Glen Plake',
-        description: 'The heart chute is one of my favorite chutes',
-        category: 'SELFIE',
-        githublogin: "gPlake"
-    },
-    {
-        id:3,
-        name:'Any person do it',
-        description: 'The heart chute is one of my favorite chutes',
-        category: 'LANDSCAPE',
-        githublogin: "sSchimidt"
-    },
-];
-
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
-    allUsers: () => users
-  },
-  Mutation: {
-    postPhoto(root, params) {
-      let newPhoto = {
-        id: _id++,
-        ...params.input
-      };
-      photos.push(newPhoto);
-
-      return newPhoto;
-    }
-  },
-  Photo: {
-    url: root => `http://letox.com/img/${root.id}.jpg`,
-    postedBy: root => users.find(u => u.githublogin === root.githublogin),
-    taggedUsers: root => tags.filter(tag => tag.photoId === root.id)
-    // Convert the array of tags into an array of userIDs
-    .map(tag => tag.userId)
-    // Converts array of userIDs into an array of user objects
-    .map(userId => users.find(u => u.githublogin === userId))
-  },
-  User: {
-    postedPhotos: root => photos.filter(p => p.githublogin === root.githublogin),
-    inPhotos: root => tags
-    // Returns an array of tags that only contain the current user
-    .filter(tag => tag.userId === root.id)
-    // Convert the array of tags into array of photoIDs
-    .map(tag => tag.photoId)
-    // Convert array of photoIDs into array of photo objects
-    .map(photoId => photos.find(p => p.id === photoId))
-  }
-};
+var app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
+// Call applyMiddleqare() to allow middleware mounted on the same path
+server.applyMiddleware({ app });
 
-server
-  .listen()
-  .then(({ url }) => console.log(`Graphql Service running on ${url}`));
+// Create a home route
+app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
+app.get('/playground', expressPlayground({endpoint: '/graphql'}))
+
+app.listen({ port: 4000 }, () =>
+  console.log(`Graphql Service running on @
+    http://localhost:4000${server.graphqlPath}
+  `)
+);
