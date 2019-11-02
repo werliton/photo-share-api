95d05d48733b0e95c93cb56d175d3c88c97c7c92
werliton <werlitoncarlos@gmail.com>
2019-11-02 12:43:48 -0300

    Adding Database to Context

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const expressPlayground = require('graphql-playground-middleware-express').default
-const { readFileSync } = require('fs')
+const { MongoClient } = require("mongodb");
+const expressPlayground = require("graphql-playground-middleware-express")
+  .default;
+const { readFileSync } = require("fs");
+require("dotenv").config();
 
-const typeDefs = readFileSync('./typeDefs.graphql', 'UFT-8')
-const resolvers = require('./resolvers')
+const typeDefs = readFileSync("./typeDefs.graphql", "UFT-8");
+const resolvers = require("./resolvers");
 
-var app = express();
+// Create Asynchronous Function
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-// Call applyMiddleqare() to allow middleware mounted on the same path
-server.applyMiddleware({ app });
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
 
-// Create a home route
-app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
-app.get('/playground', expressPlayground({endpoint: '/graphql'}))
+  const db = client.db();
 
-app.listen({ port: 4000 }, () =>
-  console.log(`Graphql Service running on @
+  const context = { db };
+
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+  // Call applyMiddleqare() to allow middleware mounted on the same path
+  server.applyMiddleware({ app });
+
+  // Create a home route
+  app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
+  app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`Graphql Service running on @
     http://localhost:4000${server.graphqlPath}
   `)
-);
+  )
+}
+
+start();
